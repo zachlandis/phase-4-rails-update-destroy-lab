d1e17aa692fa4f97e1842af00e4aea9fb8edc33b
Zach <zachlandis91@gmail.com>
2023-06-20 13:01:55 -0600

    commit tree 4c3e98815ed23aa645f4efa8c6eb1f6d48257cc3

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,30 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: {error: "Plant not found"}, status: :not_found
+    end
+  end
+
+
+
   private
 
   def plant_params
